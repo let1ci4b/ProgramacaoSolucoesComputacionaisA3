99f875247fe7a2922bc6b3eea3d751e1dfc8e807
let1ci4b <leticia_b_souza@SN-363484.edu.fiesc.com.br>
2023-05-22 15:58:37 -0300

    new commit

---
+++ src/main/java/Principal/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Principal;
+
+/**
+ *
+ * @author leticia_b_souza
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
