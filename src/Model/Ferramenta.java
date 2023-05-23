641a22d3b44383d4c5c7229270278ed3bb36972d
Renan Machado <renanpando@gmail.com>
2023-05-23 20:56:35 -0300

    update: novo construtor

--- src/Model/Ferramenta.java
+++ src/Model/Ferramenta.java
@@ -12,7 +12,13 @@ public class Ferramenta {
     public Ferramenta() {
     }
 
-    public Ferramenta(int id, String nome, String marca, double custoAquisicao) {
+    public Ferramenta(String nome, String marca, double custoAquisicao) { // construtor sem id (auto-increment)
+        this.nome = nome;
+        this.marca = marca;
+        this.custoAquisicao = custoAquisicao;
+    }
+    
+    public Ferramenta(int id, String nome, String marca, double custoAquisicao) { // construtor para selectAll
         this.id = id;
         this.nome = nome;
         this.marca = marca;
