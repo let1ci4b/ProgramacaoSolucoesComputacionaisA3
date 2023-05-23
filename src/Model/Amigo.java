2a9d1369079f34baf736e3a9a0e6b98ffc3abc5b
Renan Machado <renanpando@gmail.com>
2023-05-23 20:56:18 -0300

    update: removido métodos desnecessários

--- src/Model/Amigo.java
+++ src/Model/Amigo.java
@@ -1,9 +1,7 @@
 
 package Model;
 
-import java.util.*;
 import DAO.AmigoDAO;
-import java.sql.SQLException;
 
 public class Amigo {
     
@@ -17,14 +15,14 @@ public class Amigo {
          this.dao = new AmigoDAO(); // inicializado nao importa em qual construtor
     }
     
-    public Amigo(String nome, long telefone, int quantEmprest) {
+    public Amigo(String nome, long telefone, int quantEmprest) { // construtor sem id (auto-increment)
         this.nome = nome;
         this.telefone = telefone;
         this.quantEmprest = quantEmprest;
         this.dao = new AmigoDAO(); // inicializado nao importa em qual construtor
     }
 
-    public Amigo(int id, String nome, long telefone, int quantEmprest) {
+    public Amigo(int id, String nome, long telefone, int quantEmprest) { // construtor para selectAll
         this.id = id;
         this.nome = nome;
         this.telefone = telefone;
@@ -70,46 +68,9 @@ public class Amigo {
     public String toString() {
         return "\n ID: " + this.getId()
                 + "\n Nome: " + this.getNome()
-                + "\n Idade: " + this.getTelefone()
-                + "\n Curso: " + this.getQuantEmprest()
+                + "\n Telefone: " + this.getTelefone()
+                + "\n Quantidade de empréstimos: " + this.getQuantEmprest()
                 + "\n -----------";
-    }
-/*
-    
-        ABAIXO OS M�TODOS PARA USO JUNTO COM O DAO
-        SIMULANDO A ESTRUTURA EM CAMADAS PARA USAR COM BANCOS DE DADOS.
-    
-     */
-    // Retorna a Lista de Amigos(objetos)
-    public ArrayList getMinhaLista() {
-        return dao.getMinhaLista();
-    }
-
-    // Cadastra novo amigo
-    public boolean InsertAmigoBD(String nome, long telefone, int QuantEmprest) throws SQLException {
-        Amigo objeto = new Amigo(nome, telefone, QuantEmprest);
-        dao.InsertAmigoBD(objeto);
-        return true;
-
-    }
-
-    // Deleta um amigo especifico pelo seu campo ID
-    public boolean DeleteAmigoBD(int id) {
-        dao.DeleteAmigoBD(id);
-        return true;
-    }
-
-    // Edita um aluno espec�fico pelo seu campo ID
-    public boolean UpdateAmigoBD(String nome, long telefone, int QuantEmprest) {
-        Amigo objeto = new Amigo(nome, telefone, QuantEmprest);
-        dao.UpdateAmigoBD(objeto);
-        return true;
-    }
-
-    // carrega dados de um amigo espec�fico pelo seu ID
-    public Amigo carregaAmigo(int id) {
-        dao.carregaAmigo(id);
-        return null;
     }
     
 }
