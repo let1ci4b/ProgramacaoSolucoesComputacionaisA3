b08bd10e6d91adcf7e8d43f698a4e0ed28fd6dbb
Renan Machado <renanpando@gmail.com>
2023-05-24 01:50:26 -0300

    update: selectall adicionado (necessario testar)

--- src/DAO/EmprestimoDAO.java
+++ src/DAO/EmprestimoDAO.java
@@ -1,11 +1,12 @@
 
 package DAO;
 
+import Model.Amigo;
 import Model.Emprestimo;
+import Model.Ferramenta;
 import java.util.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -13,7 +14,7 @@ import java.sql.Statement;
 
 public class EmprestimoDAO {
     
-    public static ArrayList<Emprestimo> MinhaLista = new ArrayList<Emprestimo>();
+    public static ArrayList<Emprestimo> MinhaLista = new ArrayList<>();
         public Connection getConexao() {
 
         Connection connection = null;  //instância da conexão
@@ -47,9 +48,41 @@ public class EmprestimoDAO {
             return null;
 
         } catch (SQLException e) {
-            System.out.println("Nao foi possivel conectar...");
+            System.out.println("Nao foi possivel conectar..." + e.getMessage());
             return null;
         }
+    }
+        
+    public ArrayList getMinhaLista() {
+        AmigoDAO amigoDAO = new AmigoDAO(); // dao para transformar o id em objeto
+        FerramentaDAO ferramentaDAO = new FerramentaDAO();
+        
+        MinhaLista.clear();
+        
+        try {
+            Statement stmt = this.getConexao().createStatement();
+            ResultSet res = stmt.executeQuery("SELECT * FROM tb_emprestimos");
+            while (res.next()) {
+                
+                int id = res.getInt("id_emprestimo");
+                Amigo amigo = amigoDAO.carregaAmigo(res.getInt("fk_amigo"));
+                Ferramenta ferramenta = ferramentaDAO.carregaFerramenta(res.getInt("fk_ferramenta"));
+                java.sql.Date dataEmprestimo = res.getDate("data_emprestimo");
+                java.sql.Date dataDevolucao = res.getDate("data_devolucao");
+                boolean status = res.getBoolean("status");
+                
+                Emprestimo objeto = new Emprestimo(id, amigo, ferramenta, dataEmprestimo, dataDevolucao, status);
+                
+                MinhaLista.add(objeto);
+            }
+            
+            stmt.close();
+            
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        
+        return MinhaLista;
     }
 
 }
